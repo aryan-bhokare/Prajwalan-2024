27bd84f820c8288c1c37c5a755e7a5821c5acdfe
durgesh-firake09 <firake.durgesh876@gmail.com>
2024-01-24 09:08:05 +0530

    Configuration - Backend

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-_z-xmce_btiln43^53^gs65r1l!$0_rp7)@z+izf=3%$k085u@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# add host here
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -36,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'rest_framework',
+    "corsheaders",
     'django.contrib.staticfiles',
 ]
 
@@ -43,11 +46,14 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS: True    # development purposes
+# CORS_ALLOWED_ORIGINS = []
 
 ROOT_URLCONF = 'server.urls'
 
--- server/server/urls.py
+++ server/server/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
