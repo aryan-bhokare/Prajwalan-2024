27208cc44278ea74c05ec5cebbd6f381d3863b24
Pranaychandrikapure <pranaychandrikapure@gmail.com>
2024-01-26 14:28:10 +0530

    Adding new events card and routing using react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,18 @@ import Hero from './components/Header/Hero'
 import Event from './components/Events/Events'
 import Footer from './components/Footer/Footer'
 import Memories from './components/Memories/Memories'
+import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
   return (
     <>
+    <Router>
+
       <Navbar />
+      <Routes>
+        {/* <Route path="/" element={}/>        
+        <Route path="/about" element={}/> */}
+       </Routes>
       <Hero />
       {/* <Steps /> */}
       {/* <BestSellers /> */}
@@ -17,6 +24,7 @@ function App() {
       {/* <Collections /> */}
       <Event />
       <Footer />
+    </Router>
     </>
   )
 }
--- client/src/components/Events/EventCard.jsx
+++ client/src/components/Events/EventCard.jsx
@@ -1,32 +1,19 @@
 import React from 'react'
-import { AiOutlineArrowRight } from 'react-icons/ai'
-function EventCard({ img, title, desc }) {
+import Card_css from './EventCard.module.css'
+
+function EventCard(props) {
   return (
     <>
-      <div className='rounded-lg overflow-hidden border border-slate-700'>
-        {/* Flex Container */}
-        <div className='flex flex-col space-y-2 justify-center  rounded-t-lg'>
-          {/* Blog Image */}
-          <div className='h-48 w-full overflow-hidden '>
-            <img
-              src={img}
-              alt='blogImg'
-              className='min-h-0 object-cover h-full w-full'
-            />
-          </div>
-        </div>
-        {/* Title */}
-        <div className=' py-6 max-w-xs mx-auto flex flex-col justify-center space-y-3 p-4'>
-          <h4 className='text-lg font-medium hover:text-indigo-600 duration-300 ease-in-out hover:cursor-pointer'>
-            {title}
-          </h4>
-          <p className='text-slate-400 text-sm'>{desc}</p>
-          <div className='flex items-center space-x-2 cursor-pointer font-semibold hover:text-indigo-600 ease-in-out duration-300'>
-            <p>Read More</p>
-            <AiOutlineArrowRight size={12} />
-          </div>
-        </div>
-      </div>
+    <div className={Card_css.main}>
+
+      		<div className={Card_css.card} role="button">
+			<span className={Card_css.glow}></span>
+			<div className={Card_css.inner}>
+				<span>Events</span>GCOEA <br/>
+				{props.txt}
+			</div>
+		</div>
+    </div>
     </>
   )
 }
