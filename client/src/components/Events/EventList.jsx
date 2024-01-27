7e574336da9c3aa050d8b2a6d0eca6ba8d3a110a
Aryan <aryan1bhokare@gmail.com>
2024-01-27 17:56:46 +0530

    eventcard new added

--- client/src/components/Events/EventList.jsx
+++ client/src/components/Events/EventList.jsx
@@ -7,23 +7,23 @@ function EventList() {
   return (
     <>
       <motion.div
-        // variants={parentVariants}
-        // initial='hidden'
-        // whileInView='show'
-        // viewport={{ once: true }}
+        variants={parentVariants}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: true }}
         className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'
       >
         {/* Collection Card  */}
         {blogs.map((blog, idx) => {
           return (
-            // <motion.div variants={childVariants} key={idx}>
+            <motion.div variants={childVariants} key={idx}>
               <EventCard
                 key={blog.title}
                 img={blog.img}
                 title={blog.title}
                 desc={blog.desc}
               />
-            // </motion.div>
+            </motion.div>
           )
         })}
       </motion.div>
