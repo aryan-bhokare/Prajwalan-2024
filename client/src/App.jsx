915af19e37b5f95288f8032ba1c9a1a272431a0b
durgesh-firake09 <firake.durgesh876@gmail.com>
2024-01-28 20:09:46 +0530

    added event page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,28 +3,29 @@ import Hero from './components/Header/Hero'
 import Event from './components/Events/Events'
 import Footer from './components/Footer/Footer'
 import Memories from './components/Memories/Memories'
-import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Countdown from './components/Countdown/Countdown'
+import EventPage from './components/Events/EventPage'
 
 function App() {
 
-  const targetDate=new Date(2024, 1, 28, 12, 0, 0);
+  const targetDate = new Date(2024, 1, 28, 12, 0, 0);
   return (
     <>
-    <Router>
+      <Router>
 
-      <Navbar />
-      <Routes>
-        {/* <Route path="/" element={}/>        
-        <Route path="/about" element={}/> */}
-       </Routes>
-      
-      <Hero />
-      <Countdown targetDate={targetDate}/>
-      <Memories/>
-      <Event />
-      <Footer />
-    </Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<><Hero />
+            <Countdown targetDate={targetDate} />
+            <Memories />
+            <Event />
+          </>} />
+          <Route path="/event" element={<EventPage />} />
+        </Routes>
+        <Footer />
+
+      </Router>
     </>
   )
 }
