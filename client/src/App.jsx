8de9626f0913d45a582410a2c12f0cf5e075a8b8
durgesh-firake09 <firake.durgesh876@gmail.com>
2024-01-28 20:19:25 +0530

    updated app.jsx with event page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
-import  { useState ,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from './components/Header/Navbar'
 import Hero from './components/Header/Hero'
 import Event from './components/Events/Events'
 import Footer from './components/Footer/Footer'
 import Memories from './components/Memories/Memories'
-import { BrowserRouter ,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Countdown from './components/Countdown/Countdown'
 import Spinner from './components/Spinner';
+import EventPage from './components/Events/EventPage';
+
 function App() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
 
     const timeoutId = setTimeout(() => {
       setLoading(false);
-    },3000);
+    }, 3000);
 
     return () => clearTimeout(timeoutId);
   }, []);
-  
-  const targetDate=new Date(2024, 1, 28, 12, 0, 0);
+
+  const targetDate = new Date(2024, 1, 28, 12, 0, 0);
   return (
     <>
       {loading ? (
@@ -27,17 +29,18 @@ function App() {
         <BrowserRouter>
           <Navbar />
           <Routes>
-           <></>
+            <Route path='/' element={<><Hero />
+              <Countdown targetDate={targetDate} />
+              <Memories />
+              <Event /></>} />
+            <Route path='/event' element={<EventPage/>}/>
           </Routes>
-          <Hero />
-          <Countdown targetDate={targetDate} />
-          <Memories />
-          <Event />
+
           <Footer />
         </BrowserRouter>
       )}
     </>
   );
- 
+
 }
 export default App
